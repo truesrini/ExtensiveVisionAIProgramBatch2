ff45d21a703a635222d3497f2a0ae71d2a902b8f
Srinivasan Govindaraj <55595414+truesrini@users.noreply.github.com>
2020-05-24 02:56:19 +0530

    Add files via upload

---
+++ Session8 Assignment/srini_dataloader.py
@@ -0,0 +1,27 @@
+# This module contains the data loading functionality for Cifar10 dataset. This module returns the train loader and test loader
+
+import torch
+from torchvision import datasets, transforms
+import torchvision
+
+
+def cifar10_loader() :
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=128,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+               
+    return trainloader, testloader
