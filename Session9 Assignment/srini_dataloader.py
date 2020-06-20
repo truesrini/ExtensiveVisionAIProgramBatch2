250040dda4d095829e7552decd00a014b6da3400
Srinivasan Govindaraj <truesrini@gmail.com>
2020-06-20 12:52:01 +0530

    Checked in Data Loader and ReadMe

--- Session9 Assignment/srini_dataloader.py
+++ Session9 Assignment/srini_dataloader.py
@@ -3,6 +3,7 @@
 import torch
 from torchvision import datasets, transforms
 import torchvision
+import numpy as np
 
 from albumentations import (
     HorizontalFlip, IAAPerspective, ShiftScaleRotate, CLAHE, RandomRotate90,
@@ -45,7 +46,6 @@ def cifar10_loader_albumentation(HorizontalFlip_p = 0, IAAPerspective_p = 0, Shi
             
         def __call__(self,img):
             img = np.array(img)
-            img = self.transforms(image=img)["image"]
             return img
 
     transform_train = Compose([
@@ -85,12 +85,12 @@ def cifar10_loader_albumentation(HorizontalFlip_p = 0, IAAPerspective_p = 0, Shi
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=albu_transforms(transform_train))
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
-                                              shuffle=True, num_workers=2)
+                                              shuffle=True, num_workers=1)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=albu_transforms(transform_test))
     testloader = torch.utils.data.DataLoader(testset, batch_size=128,
-                                             shuffle=False, num_workers=2)
+                                             shuffle=False, num_workers=1)
 
     classes = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
